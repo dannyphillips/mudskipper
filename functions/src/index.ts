2db7b0f0277527f3f9d5ae091fe3b99ceaa2135e
Danny Phillips <danny.phillips8@gmail.com>
2019-05-09 23:27:18 -0700

    Functions build, but arent quite working right:

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,66 @@
 import * as functions from 'firebase-functions';
+const Crawler = require('crawler');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.mudskipper = functions.https.onRequest((request, response) => {
+  const c = new Crawler({
+    maxConnections: 10,
+    // This will be called for each crawled page
+    callback: function (error, res) {
+      if(error) {
+        console.log(error);
+      } else {
+        const $ = res.$;
+        const rows = $(".table-fishcount")
+          .find("tr")
+          .not(".catch-row");
+        const data =[];
+        rows.each(rowIndex => {
+          const row = rows[rowIndex];
+          const date = $("td", row)
+            .first()
+            .text();
+          const tripType = $(".triptype", row).text();
+          const anglers = $(".anglers", row).text();
+          const fishCounts = $(".catch", row).children();
+          const fish = [];
+          fishCounts.each(fishIndex => {
+            const fishCount = $(".count", fishCounts[fishIndex]).text();
+            const fishType = $(".fishtype", fishCounts[fishIndex]).text();
+            fish.push({
+              name: fishType,
+              count: fishCount
+            });
+          });
+          data.push({
+            date: date,
+            tripType: tripType,
+            anglers: anglers,
+            fish: fish
+          })
+        });
+        data.shift();
+        response.send(data);
+        console.log(data);
+      }
+    }
+
+  });
+  function twoDigits(num) {
+    if (num >= 10) {
+      return num.toString();
+    } else {
+      return "0" + num.toString();
+    }
+  }
+
+  const today = new Date();
+  const year = today.getFullYear().toString();
+  const month = today.getMonth() + 1;
+  const day = today.getDate().toString();
+  const dateInt = year + twoDigits(month) + twoDigits(day);
+
+  c.queue(`https://www.fishingreport.us/plugin/page/16/${dateInt}`);
+});
